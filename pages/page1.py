578cbf2b346bd7628e42454b610f847ef7e62c39
mimir <55585693+AntaresBermuda@users.noreply.github.com>
2025-01-07 21:32:11 +0000

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -5,7 +5,4 @@ st.title("The ledger")
 
 ledger = pd.read_csv("ledger.csv")
 
-st.dataframe(ledger.style.set_properties(**{'max-width': '200px'}))
-st.dataframe(ledger.style.set_properties(**{'min-width': '200px'}))
-
-st.dataframe(ledger)
+st.dataframe(ledger.style.set_properties(**{'max-width': '2px'}))
