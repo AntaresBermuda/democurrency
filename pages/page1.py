e22871e313890384b56439470ed435968956f06c
mimir <55585693+AntaresBermuda@users.noreply.github.com>
2025-01-07 22:19:33 +0000

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -17,3 +17,22 @@ b64 = base64.b64encode(csv.encode()).decode()
 file_name = "data.csv"
 href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
 st.markdown(href, unsafe_allow_html=True)
+
+
+
+# Save the CSV to a temporary file
+temp_file_path = 'temp_ledger.csv'
+with open(temp_file_path, 'w') as f:
+    f.write(csv)
+
+# Create the download button
+with open(temp_file_path, "rb") as file:
+    st.download_button(
+        label="Download Ledger CSV",
+        data=file,
+        file_name="ledger.csv",
+        mime="text/csv",
+    )
+
+# Remove the temporary file after download
+os.remove(temp_file_path)
