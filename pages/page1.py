18de7054b18da48485e168d366a180feee0a189a
mimir <55585693+AntaresBermuda@users.noreply.github.com>
2025-01-07 22:58:16 +0000

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -11,14 +11,6 @@ st.dataframe(ledger.style.set_properties(**{'max-width': '2px'}))
 
 csv = ledger.to_csv(index=False)
 
-# Encode the CSV string as base64
-b64 = base64.b64encode(csv.encode()).decode()
-
-# Create a downloadable link for the CSV
-file_name = "data.csv"
-href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
-st.markdown(href, unsafe_allow_html=True)
-
 
 # Save the CSV to a temporary file
 temp_file_path = 'temp_ledger.csv'
